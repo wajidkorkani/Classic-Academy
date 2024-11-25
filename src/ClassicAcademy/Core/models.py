4b5ecff285bfcea6b12352ea59bef0454c18772f
wajidkorkani <errormrcoder@gmail.com>
2024-11-25 11:31:08 +0500

    Models are named.

--- src/ClassicAcademy/Core/models.py
+++ src/ClassicAcademy/Core/models.py
@@ -4,42 +4,78 @@ from django.db import models
 class Coaching_Classes(models.Model):
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Coaching_Subjects(models.Model):
     name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
 
 class English_Courses(models.Model):
     topic = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.topic
+
 class Courses(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
 
 class Facilties(models.Model):
     name = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Education_Programs(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class MS_Two_Year_Programs(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class BS_Four_Year_Programs(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class MBA(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Two_Year_Associate_Degree_Programs(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
 class Diploma_Programs(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.name
+
 class Specialization_Certificate_Six_Months_Courses_Programs(models.Model):
     name = models.CharField(max_length=250)
     duration = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
